d75038055de5e33d7a6c616a7b999c623782e4e3
Vishwa <vishwahegadihal286@gmail.com>
2026-09-10 17:36:13 +0000

    Add pattern problems

---
+++ p1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k,row;
+
+    printf("Enter no of rows : \n");
+    scanf("%d",&row);
+
+    for(i=0;i<row;i++)
+        {
+            for(j=row-i;j>0;j--)
+                {
+                    printf(" ");
+                }
+            for(k=0;k<=i;k++)
+                {
+                    printf("* ");
+                }
+            printf("\n");
+        }
+    for(i=1;i<row;i++)
+        {
+            for(j=0;j<=i;j++)
+                {
+                    printf(" ");
+                }
+            for(k=row-i;k>0;k--)
+                {
+                    printf("* ");
+                }
+            printf("\n");
+        }
+}
+
